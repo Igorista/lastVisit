4c3e859c6a0c94be0b26f635fcb98a44a093485b
MaIgor <mybox-08@ya.ru>
2021-12-05 01:27:32 +0300

    changes in code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,48 +1,46 @@
-const val MINUTE = "минуту"
-const val MINUTES1 = "минут"
-const val MINUTES2 = "минуты"
-const val HOUR = "час"
-const val HOURS1 = "часа"
-const val HOURS2 = "часов"
-const val SECONDS: Int = 172800
-const val NAME: String = "Иван"
-
 fun main(args: Array<String>) {
-    agoToText()
+    val name: String = "Иван"
+    val time: Int = 1800
+    println("$name ${agoToText(time)}")
 }
 
-fun correctMin(): String {
+fun correctMin(seconds: Int): String {
     val time: String
-    val min: Int = SECONDS / 60
-    when (true) {
-        min % 10 == 1 && min % 100 != 11  -> time = MINUTE
-        min % 10 == 0 -> time = MINUTES1
-        min % 100 >= 11 && min <= 19 -> time = MINUTES1
-        min % 10 >= 2 && min % 10 <= 4 -> time = MINUTES2
-        else -> time = MINUTES1
+    val min: Int = seconds / 60
+    when {
+        min % 10 == 1 && min % 100 != 11 -> time = "минуту"
+        min % 10 == 0 -> time = "минут"
+        min % 100 >= 11 && min <= 19 -> time = "минут"
+        min % 10 in 2..4 -> time = "минуты"
+        else -> time = "минут"
     }
     return time
 }
-fun correctHour(): String {
+
+fun correctHour(seconds: Int): String {
     val time: String
-    val hour: Int = SECONDS / 3600
-    when (true) {
-        hour % 10 == 10 -> time = HOUR
-        hour % 10 == 0 -> time = HOURS2
-        hour % 10 >= 2 && hour % 10 <= 4 -> time = HOURS1
-        else -> time = HOURS2
+    val hour: Int = seconds / 3600
+    when {
+        hour % 10 == 10 -> time = "час"
+        hour % 10 == 0 -> time = "часов"
+        hour % 10 in 2..4 -> time = "часа"
+        else -> time = "часов"
     }
     return time
 }
-fun agoToText() {
-    val min: Int = SECONDS / 60
-    val hour: Int = SECONDS / 3600
-    when (true) {
-        SECONDS >= 0 && SECONDS <= 60 -> println("$NAME был(а) только что")
-        SECONDS >= 61 && SECONDS <= 60 * 60 -> println("$NAME был(а) $min ${correctMin()} назад ")
-        SECONDS >= 60 * 60 + 1 && SECONDS < 24 * 60 * 60 -> println("$NAME был(а) $hour ${correctHour()} назад ")
-        SECONDS >= 24 * 60 * 60 && SECONDS < 24 * 60 * 60 * 2 -> println("$NAME был(а) сегодня")
-        SECONDS >= 24 * 60 * 60 * 2 && SECONDS < 24 * 60 * 60 * 3 -> println("$NAME был(а) вчера")
-        else -> println("$NAME был(а) давно")
-    }
+
+fun agoToText(seconds: Int): String {
+    val min: Int = seconds / 60
+    val hour: Int = seconds / 3600
+    val visit: String =
+        when {
+            seconds in 0..60 -> "был(а) только что"
+            seconds >= 61 && seconds <= 60 * 60 -> "был(а) $min ${correctMin(seconds)} назад "
+            seconds >= 60 * 60 + 1 && seconds < 24 * 60 * 60 -> "был(а) $hour ${correctHour(seconds)} назад "
+            seconds >= 24 * 60 * 60 && seconds < 24 * 60 * 60 * 2 -> "был(а) сегодня"
+            seconds >= 24 * 60 * 60 * 2 && seconds < 24 * 60 * 60 * 3 -> "был(а) вчера"
+            else -> "был(а) давно"
+        }
+    return visit
+
 }
